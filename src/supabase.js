b12966ef0a60ce463f9b04806114209910123deb
drewglassman8-gh <drewglassman8@gmail.com>
2026-05-18 12:58:18 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const url = "https://hzhvwjtnwvhtrczestzey.supabase.co";
-const key = "sb_publishable_4g3mxglhmqKQhyvrhp22gQ_IWmGrAfd";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6aHZ3anRud3ZodHJjenN0emV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNTM2NTYsImV4cCI6MjA5NDYyOTY1Nn0.2mL4y2o_CobnoRuB5qOgs1PWKDw0ZeTNs1SgE6iSf48";
 
 export const supabase = createClient(url, key);
